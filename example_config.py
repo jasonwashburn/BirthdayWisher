bd11306a029e57ab839e7d7d79b3427f3497425c
Jason Washburn <wburn@wburn.net>
2021-03-16 07:01:03 -0500

    added docstrings and example config file

---
+++ example_config.py
@@ -0,0 +1,5 @@
+"""
+Rename this file to config.py and enter your gmail credentials below
+"""
+my_email = "example@example.com"
+password = "password"
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+A script that sends a randomly chosen, customized birthday message to contacts listed in birthdays.csv
+"""
 import smtplib
 import datetime as dt
 import config  # Import secret login information, stored separately to keep it out of git/github
@@ -6,13 +9,15 @@ import pandas as pd
 
 
 def send_email(to_address, subject, message):
-    # Sends an email message with gmail using the account info provided in config.py
-    #
-    # args:
-    #   to_address: email address of the recipient
-    #   subject: subject of email
-    #   message: message to send
-    #
+    """
+    Sends an email message with gmail using the account info provided in config.py
+
+    :param to_address: email address of the recipient
+    :param subject: subject of email
+    :param message: message to send
+    :return: None
+    """
+
     with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
         connection.login(user=config.my_email, password=config.password)
@@ -23,11 +28,15 @@ def send_email(to_address, subject, message):
         )
 
 
+# Get the current date
 now = dt.datetime.now()
 
-birthdays_df = pd.read_csv("birthdays.csv")
+birthdays_df = pd.read_csv("birthdays.csv")  # Read in birthdays from birthdays.csv file
+
+# Create a new dataframe containing birthdays that match today's month and day
 todays_birthdays_df = birthdays_df[(birthdays_df.month == now.month) & (birthdays_df.day == now.day)]
 
+# Iterate through the dataframe of matching birthdays then customize and send a randomly chosen message for each
 for index, row in todays_birthdays_df.iterrows():
     with open(f"letter_templates/letter_{random.choice([1, 2, 3])}.txt") as letter_file:
         custom_message = letter_file.read()
